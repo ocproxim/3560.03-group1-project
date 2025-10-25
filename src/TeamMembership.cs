4ec91c3dd55058e79b57779e2ab5609734bbc61d
Nathmathan <160674481+Nathmathan@users.noreply.github.com>
2025-10-25 00:01:18 -0700

    get/set methods for TeamMembership

--- src/TeamMembership.cs
+++ src/TeamMembership.cs
@@ -5,4 +5,12 @@ public class TeamMembership
     public required String membershipID;
     public required String Season;
     public required int jerseyNumber;
+
+    // Get methods
+    public String getSeason() { return Season; }
+    public int getJerseyNumber() { return jerseyNumber; }
+
+    // Set methods
+    public void setSeason(String newSeason) { Season = newSeason; }
+    public void setJerseyNumber(int newJerseyNumber) { jerseyNumber = newJerseyNumber; }
 }
