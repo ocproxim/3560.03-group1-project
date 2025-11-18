08705e1bfda3c391ceecbb46031430fd0afdc2df
RYAN-PC\Ryan <davies.c.ryan@gmail.com>
2025-11-17 21:03:37 -0800

    preliminary WebSocket implementation, JS listener, implemented preliminary search filters

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+HttpListener listener = new();
+listener.Prefixes.Add("http://localhost:8080/");
+listener.Start();
+Console.WriteLine("WebSocket server running on ws://localhost:8080 ...");
+
+while (true)
+{
+    HttpListenerContext context = await listener.GetContextAsync();
+
+    if (context.Request.IsWebSocketRequest)
+    {
+        WebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
+        WebSocket socket = wsContext.WebSocket;
+        Console.WriteLine("Client connected!");
+
+        byte[] buffer = new byte[1024];
+
+        while (socket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                Console.WriteLine("Client disconnected.");
+                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                break;
+            }
+
+            string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Console.WriteLine($"Received: {message}");
+
+            var queryData = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(message);
+
+            // TODO: Query your database here using `message`
+
+            string response = $"Received: {message}";
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+            await socket.SendAsync(responseBytes, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+        context.Response.Close();
+    }
+}
--- src/UI/main.js
+++ src/UI/main.js
@@ -1,6 +1,18 @@
+//conect to websocket server
+const socket = new WebSocket("ws://localhost:8080");
+
+socket.addEventListener('open', () => {
+    console.log("Connected to database server");
+});
+
+socket.addEventListener('message', (event) => {
+    console.log("Message from server:", event.data);
+});
+
+
+//get string input
 const searchForm = document.getElementById('search');
 const searchInput = document.getElementById('searchInput');
-const outputDisplay = document.getElementById('output');
 
 searchForm.addEventListener('submit', function(event) {
     // stop page reload
@@ -8,10 +20,25 @@ searchForm.addEventListener('submit', function(event) {
 
     // get the search input
     const inputString = searchInput.value;
+    //get dropdown input
+    const searchSportForm = document.getElementById('sportType');
+    const searchTypeForm = document.getElementById('searchType');
 
-    // print to console
-    console.log('User entered string:', inputString);
+    //build JSON
+    const message = {
+        query: inputString,
+        sport: searchSportForm.value,
+        type: searchTypeForm.value
+    };
+
+    //send to websocket server
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(message));
+        console.log("Sent:", message);
+    }
 
     // clear the input field
     searchInput.value = '';
+    searchSportForm.value = '';
+    searchTypeForm.value = '';
 });
